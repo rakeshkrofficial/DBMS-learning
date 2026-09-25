8533e8891ee4166148f5aba8274e0c325e532ef8
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-09-25 23:40:47 +0530

    Add files via upload

---
+++ practics_1.sql
@@ -0,0 +1,16 @@
+create database xyz_company;
+use xyz_company;
+create table employee(
+	id int primary key,
+    name varchar(50),
+    salary int 
+);
+
+insert into employee
+(id,name,salary)
+values
+(1,"adam",25000),
+(2,"bob",30000),
+(3,"casey",40000);
+
+select * from employee;
